04dde19e706fe764fa18e9d90c5643140f166891
carloscastrofilho <cpcftq@gmail.com>
2023-08-21 11:09:51 -0300

    aula 2

--- App.js
+++ App.js
@@ -1,10 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Header from "./Header"
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Header />
+      <Header />
+      <Header />
+      <Header />
+      <Text style={styles.title}>aula 2</Text>
+      <Text style={styles.title2}>/Fundamentos</Text>
+      <Header />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +20,18 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#ff0',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  title: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    color: 'black'
+  },
+  title2: {
+    fontSize: 50,
+    fontWeight: 'bold',
+    color: 'red'
+  }
 });
